fd446d13b34e496d0028781909b04146488beb19
keithpotter21 <keith.potter@kccd.edu>
2017-03-08 17:58:51 -0800

    changes

--- routes/schedule.js
+++ routes/schedule.js
@@ -7,15 +7,19 @@ router.get('/', function(req, res, next) {
   var db = firebase.database();
   var ref = db.ref("/schedule/games");
 
-  ref.once("value")
+  ref.orderByChild("timestamp")
+      .once("value")
       .then(function(snapshot) {
-        var data = snapshot.val();
-        if(data) {
-          console.log(data);
-          res.render('schedule', {title: 'Mad Squabbles - Schedule', pageTitle: 'Spring 2017 Full Schedule', schedule: data});
-        } else {
-          res.status(401).json({error: 'No games found'});
-        }
+          var data = [];
+          snapshot.forEach(function(child) {
+              data.push(child.val());
+          });
+          if(data) {
+              console.log(data);
+              res.render('schedule', {title: 'Mad Squabbles - Schedule', pageTitle: 'Spring 2017 Full Schedule', schedule: data});
+          } else {
+              res.status(401).json({error: 'No games found'});
+          }
       });
 });
 
